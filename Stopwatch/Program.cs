bea984cc7bc32b3198635f7d226102a99a3622f5
Carlos Daniel <carlos.oficial.ti@gmail.com>
2024-07-30 09:24:10 -0300

    Usando Substring

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -7,12 +7,22 @@ namespace Stopwatch {
     public class Program {
         static void Main(string[] args) {
 
-            Start();
+            Menu();
 
         }
 
-        static void Start() {
-            int time = 10;
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto = > 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar ?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            Console.WriteLine(data);
+        }
+        static void Start(int time) {
             int currentTime = 0;
 
             while (currentTime != time) {
@@ -21,7 +31,14 @@ namespace Stopwatch {
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado!");
+            Thread.Sleep(1000);
+
         }
+
+
+
 
     }
 }
