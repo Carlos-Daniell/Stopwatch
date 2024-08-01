c2e7f4251088d2e6a472e410b95da0612830e3b6
Carlos Daniel <carlos.oficial.ti@gmail.com>
2024-08-01 13:43:33 -0300

    Finaly

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -20,7 +20,14 @@ namespace Stopwatch {
 
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length - 1, 1));
-            Console.WriteLine(data);
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+            if (type == 'm')
+                multiplier = 60;
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
         }
         static void Start(int time) {
             int currentTime = 0;
@@ -34,6 +41,7 @@ namespace Stopwatch {
             Console.Clear();
             Console.WriteLine("Stopwatch finalizado!");
             Thread.Sleep(1000);
+            Menu();
 
         }
 
